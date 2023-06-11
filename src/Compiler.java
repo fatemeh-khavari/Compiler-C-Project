cd0e8ee9df34cf6a5acca605b896274cdbbe9889
fatemeh-khavari <fatemehkhavari00281@gmail.com>
2023-06-11 23:41:58 +0330

    add compiler class

---
+++ src/Compiler.java
@@ -0,0 +1,31 @@
+import gen.*;
+import org.antlr.runtime.TokenSource;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.io.IOException;
+
+public class Compiler {
+    public static void main(String[] args) throws IOException {
+
+        CharStream stream =  CharStreams.fromFileName("./test/test1.c");
+        CLexer lexer = new CLexer(stream);
+        TokenStream tokens = new CommonTokenStream(lexer);
+        CParser parser = new CParser(tokens);
+        parser.setBuildParseTree(true);
+        ParseTree parseTree = parser.primaryExpression();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        CListener listener = new ProgramPase1();
+
+        walker.walk(listener, parseTree);
+
+
+
+
+
+    }
+}
