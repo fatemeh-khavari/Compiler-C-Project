d12dbc8c084b22b3e9ca6d2ae07294b99df7d187
Hamedtabei <87038694+Hamedtabei@users.noreply.github.com>
2023-07-05 16:32:25 +0330

    function call

---
+++ ProgramPase1.java
@@ -0,0 +1,845 @@
+import gen.CListener;
+import gen.CParser;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import java.util.List;
+import java.util.ListIterator;
+
+public class ProgramPase1 implements CListener {
+    int tabNum = 0;
+    List<String> functoin_names ;
+    void printTab(){
+        for(int i = 1; i <= tabNum; i++){
+            System.out.print("    ");
+        }
+    }
+
+    @Override
+    public void enterExternalDeclaration(CParser.ExternalDeclarationContext ctx) {
+
+        System.out.println("program start { ");
+        tabNum++;
+
+    }
+
+    @Override
+    public void exitExternalDeclaration(CParser.ExternalDeclarationContext ctx) {
+        tabNum--;
+        System.out.println("}");
+    }
+
+    @Override
+    public void enterFunctionDefinition(CParser.FunctionDefinitionContext ctx) {
+
+
+        String type = ctx.typeSpecifier().getText();
+        String describeFunc = ctx.declarator().getText();
+        int indexOFWriteParen = describeFunc.indexOf('(');
+        String nameFunc;
+        if(!describeFunc.startsWith("const") || !describeFunc.startsWith("*")){
+             nameFunc = describeFunc.substring(0, indexOFWriteParen);
+        }
+        else if(describeFunc.startsWith("const")){
+             nameFunc = describeFunc.substring(4, indexOFWriteParen);
+        }
+        else if(describeFunc.startsWith("*const")){
+             nameFunc = describeFunc.substring(5, indexOFWriteParen);
+        }
+        else {
+             nameFunc = describeFunc.substring(1, indexOFWriteParen);
+        }
+
+
+        if(type.equals("void")){
+            type = "return type: void(no return) ";
+        }
+        else {
+            type = "return type: " + type + " ";
+        }
+
+        //bedast avardan list function haye code
+        if(!functoin_names.contains(nameFunc))
+            functoin_names.add(nameFunc);
+        else
+
+
+
+        printTab();
+        if (describeFunc.matches("^main\\(?\\)?.*")){
+            System.out.println("main metod: " + type  +"{");
+        }
+        else {
+            System.out.println("normal metod: " + "name: "+ nameFunc +"/ " + type + "{");
+        }
+        tabNum++;
+    }
+
+    @Override
+    public void exitFunctionDefinition(CParser.FunctionDefinitionContext ctx) {
+        printTab();
+        System.out.println("}");
+        tabNum--;
+    }
+
+    @Override
+    public void enterParameterTypeList(CParser.ParameterTypeListContext ctx) {
+        printTab();
+        System.out.print("parameter list: [");
+
+        String[] param = ctx.parameterList().getText().split(",");
+        //String[] list = new String[param.length];
+        //System.out.print( "" + ctx.parameterList().parameterDeclaration(0).declarator().directDeclarator().Identifier()
+          //      + " " +ctx.parameterList().parameterDeclaration(0).declarationSpecifiers().getText());
+        for(int i = 0; i < param.length; i++){
+            String temp  = ctx.parameterList().parameterDeclaration(i).declarator().directDeclarator().Identifier()
+                    + " " +ctx.parameterList().parameterDeclaration(i).declarationSpecifiers().getText();
+            if (i != param.length -1){
+                temp = temp + ", ";
+            }
+            System.out.print(temp);
+        }
+
+
+    }
+
+    @Override
+    public void exitParameterTypeList(CParser.ParameterTypeListContext ctx) {
+
+        System.out.println("]");
+    }
+
+    @Override
+    public void enterDeclaration(CParser.DeclarationContext ctx) {
+        String type = ctx.declarationSpecifiers().getText();
+        List<CParser.InitDeclaratorContext> fields = ctx.initDeclaratorList().initDeclarator();
+
+        for (int i = 0; i < fields.size(); i++){
+            CParser.DirectDeclaratorContext name = ctx.initDeclaratorList().initDeclarator(i).declarator().directDeclarator();
+            String name_var = name.Identifier().getText();
+            printTab();
+            System.out.print("field: " + name_var + "/ type: " + type);
+            List<TerminalNode> c = name.Constant();
+            if (c.size() != 0){
+                System.out.print("/ length: "+ c.get(0)+"\n");
+            }else {
+                System.out.print("\n");
+            }
+        }
+    }
+
+    @Override
+    public void exitDeclaration(CParser.DeclarationContext ctx) {
+
+    }
+    @Override
+    public void enterPrimaryExpression(CParser.PrimaryExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitPrimaryExpression(CParser.PrimaryExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterPostfixExpression(CParser.PostfixExpressionContext ctx) {
+        //function call
+        String prob_func_cal= ctx.primaryExpression().getText();
+        if(functoin_names.contains(prob_func_cal) && ctx.LeftParen() !=  NULL ){
+            System.out.print("function call: name: "+ prob_func_cal+"/ ");
+            ListIterator<CParser.ArgumentExpressionListContext> params_itr= ctx.argumentExpressionList().listIterator();
+            System.out.print("params:");
+        while(params_itr.hasNext()){
+            ctx.argumentExpressionList().toString();
+            params_itr.next();
+            System.out.print( params_name.to);
+        }
+            System.out.print("params:"+ params_name.to);
+        }
+
+    }
+
+    @Override
+    public void exitPostfixExpression(CParser.PostfixExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterArgumentExpressionList(CParser.ArgumentExpressionListContext ctx) {
+
+    }
+
+    @Override
+    public void exitArgumentExpressionList(CParser.ArgumentExpressionListContext ctx) {
+
+    }
+
+    @Override
+    public void enterUnaryExpression(CParser.UnaryExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitUnaryExpression(CParser.UnaryExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterUnaryOperator(CParser.UnaryOperatorContext ctx) {
+
+    }
+
+    @Override
+    public void exitUnaryOperator(CParser.UnaryOperatorContext ctx) {
+
+    }
+
+    @Override
+    public void enterCastExpression(CParser.CastExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitCastExpression(CParser.CastExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterMultiplicativeExpression(CParser.MultiplicativeExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitMultiplicativeExpression(CParser.MultiplicativeExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterAdditiveExpression(CParser.AdditiveExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitAdditiveExpression(CParser.AdditiveExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterShiftExpression(CParser.ShiftExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitShiftExpression(CParser.ShiftExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterRelationalExpression(CParser.RelationalExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitRelationalExpression(CParser.RelationalExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterEqualityExpression(CParser.EqualityExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitEqualityExpression(CParser.EqualityExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterAndExpression(CParser.AndExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitAndExpression(CParser.AndExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterExclusiveOrExpression(CParser.ExclusiveOrExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitExclusiveOrExpression(CParser.ExclusiveOrExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterInclusiveOrExpression(CParser.InclusiveOrExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitInclusiveOrExpression(CParser.InclusiveOrExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterLogicalAndExpression(CParser.LogicalAndExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitLogicalAndExpression(CParser.LogicalAndExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterLogicalOrExpression(CParser.LogicalOrExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitLogicalOrExpression(CParser.LogicalOrExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterConditionalExpression(CParser.ConditionalExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitConditionalExpression(CParser.ConditionalExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterAssignmentExpression(CParser.AssignmentExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitAssignmentExpression(CParser.AssignmentExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterAssignmentOperator(CParser.AssignmentOperatorContext ctx) {
+
+    }
+
+    @Override
+    public void exitAssignmentOperator(CParser.AssignmentOperatorContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpression(CParser.ExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpression(CParser.ExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterConstantExpression(CParser.ConstantExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitConstantExpression(CParser.ConstantExpressionContext ctx) {
+
+    }
+
+
+
+    @Override
+    public void enterDeclarationSpecifiers(CParser.DeclarationSpecifiersContext ctx) {
+
+    }
+
+    @Override
+    public void exitDeclarationSpecifiers(CParser.DeclarationSpecifiersContext ctx) {
+
+    }
+
+    @Override
+    public void enterDeclarationSpecifier(CParser.DeclarationSpecifierContext ctx) {
+
+    }
+
+    @Override
+    public void exitDeclarationSpecifier(CParser.DeclarationSpecifierContext ctx) {
+
+    }
+
+    @Override
+    public void enterInitDeclaratorList(CParser.InitDeclaratorListContext ctx) {
+
+    }
+
+    @Override
+    public void exitInitDeclaratorList(CParser.InitDeclaratorListContext ctx) {
+
+    }
+
+    @Override
+    public void enterInitDeclarator(CParser.InitDeclaratorContext ctx) {
+
+    }
+
+    @Override
+    public void exitInitDeclarator(CParser.InitDeclaratorContext ctx) {
+
+    }
+
+    @Override
+    public void enterStorageClassSpecifier(CParser.StorageClassSpecifierContext ctx) {
+
+    }
+
+    @Override
+    public void exitStorageClassSpecifier(CParser.StorageClassSpecifierContext ctx) {
+
+    }
+
+    @Override
+    public void enterTypeSpecifier(CParser.TypeSpecifierContext ctx) {
+
+    }
+
+    @Override
+    public void exitTypeSpecifier(CParser.TypeSpecifierContext ctx) {
+
+    }
+
+    @Override
+    public void enterStructOrUnionSpecifier(CParser.StructOrUnionSpecifierContext ctx) {
+
+    }
+
+    @Override
+    public void exitStructOrUnionSpecifier(CParser.StructOrUnionSpecifierContext ctx) {
+
+    }
+
+    @Override
+    public void enterStructOrUnion(CParser.StructOrUnionContext ctx) {
+
+    }
+
+    @Override
+    public void exitStructOrUnion(CParser.StructOrUnionContext ctx) {
+
+    }
+
+    @Override
+    public void enterStructDeclarationList(CParser.StructDeclarationListContext ctx) {
+
+    }
+
+    @Override
+    public void exitStructDeclarationList(CParser.StructDeclarationListContext ctx) {
+
+    }
+
+    @Override
+    public void enterStructDeclaration(CParser.StructDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void exitStructDeclaration(CParser.StructDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void enterSpecifierQualifierList(CParser.SpecifierQualifierListContext ctx) {
+
+    }
+
+    @Override
+    public void exitSpecifierQualifierList(CParser.SpecifierQualifierListContext ctx) {
+
+    }
+
+    @Override
+    public void enterStructDeclaratorList(CParser.StructDeclaratorListContext ctx) {
+
+    }
+
+    @Override
+    public void exitStructDeclaratorList(CParser.StructDeclaratorListContext ctx) {
+
+    }
+
+    @Override
+    public void enterStructDeclarator(CParser.StructDeclaratorContext ctx) {
+
+    }
+
+    @Override
+    public void exitStructDeclarator(CParser.StructDeclaratorContext ctx) {
+
+    }
+
+    @Override
+    public void enterEnumSpecifier(CParser.EnumSpecifierContext ctx) {
+
+    }
+
+    @Override
+    public void exitEnumSpecifier(CParser.EnumSpecifierContext ctx) {
+
+    }
+
+    @Override
+    public void enterEnumeratorList(CParser.EnumeratorListContext ctx) {
+
+    }
+
+    @Override
+    public void exitEnumeratorList(CParser.EnumeratorListContext ctx) {
+
+    }
+
+    @Override
+    public void enterEnumerator(CParser.EnumeratorContext ctx) {
+
+    }
+
+    @Override
+    public void exitEnumerator(CParser.EnumeratorContext ctx) {
+
+    }
+
+    @Override
+    public void enterEnumerationConstant(CParser.EnumerationConstantContext ctx) {
+
+    }
+
+    @Override
+    public void exitEnumerationConstant(CParser.EnumerationConstantContext ctx) {
+
+    }
+
+    @Override
+    public void enterTypeQualifier(CParser.TypeQualifierContext ctx) {
+
+    }
+
+    @Override
+    public void exitTypeQualifier(CParser.TypeQualifierContext ctx) {
+
+    }
+
+    @Override
+    public void enterDeclarator(CParser.DeclaratorContext ctx) {
+
+    }
+
+    @Override
+    public void exitDeclarator(CParser.DeclaratorContext ctx) {
+
+    }
+
+    @Override
+    public void enterDirectDeclarator(CParser.DirectDeclaratorContext ctx) {
+
+    }
+
+    @Override
+    public void exitDirectDeclarator(CParser.DirectDeclaratorContext ctx) {
+
+    }
+
+    @Override
+    public void enterNestedParenthesesBlock(CParser.NestedParenthesesBlockContext ctx) {
+
+    }
+
+    @Override
+    public void exitNestedParenthesesBlock(CParser.NestedParenthesesBlockContext ctx) {
+
+    }
+
+    @Override
+    public void enterPointer(CParser.PointerContext ctx) {
+
+    }
+
+    @Override
+    public void exitPointer(CParser.PointerContext ctx) {
+
+    }
+
+    @Override
+    public void enterTypeQualifierList(CParser.TypeQualifierListContext ctx) {
+
+    }
+
+    @Override
+    public void exitTypeQualifierList(CParser.TypeQualifierListContext ctx) {
+
+    }
+
+
+
+    @Override
+    public void enterParameterList(CParser.ParameterListContext ctx) {
+
+    }
+
+    @Override
+    public void exitParameterList(CParser.ParameterListContext ctx) {
+
+    }
+
+    @Override
+    public void enterParameterDeclaration(CParser.ParameterDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void exitParameterDeclaration(CParser.ParameterDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void enterIdentifierList(CParser.IdentifierListContext ctx) {
+
+    }
+
+    @Override
+    public void exitIdentifierList(CParser.IdentifierListContext ctx) {
+
+    }
+
+    @Override
+    public void enterTypeName(CParser.TypeNameContext ctx) {
+
+    }
+
+    @Override
+    public void exitTypeName(CParser.TypeNameContext ctx) {
+
+    }
+
+    @Override
+    public void enterTypedefName(CParser.TypedefNameContext ctx) {
+
+    }
+
+    @Override
+    public void exitTypedefName(CParser.TypedefNameContext ctx) {
+
+    }
+
+    @Override
+    public void enterInitializer(CParser.InitializerContext ctx) {
+
+    }
+
+    @Override
+    public void exitInitializer(CParser.InitializerContext ctx) {
+
+    }
+
+    @Override
+    public void enterInitializerList(CParser.InitializerListContext ctx) {
+
+    }
+
+    @Override
+    public void exitInitializerList(CParser.InitializerListContext ctx) {
+
+    }
+
+    @Override
+    public void enterDesignation(CParser.DesignationContext ctx) {
+
+    }
+
+    @Override
+    public void exitDesignation(CParser.DesignationContext ctx) {
+
+    }
+
+    @Override
+    public void enterDesignatorList(CParser.DesignatorListContext ctx) {
+
+    }
+
+    @Override
+    public void exitDesignatorList(CParser.DesignatorListContext ctx) {
+
+    }
+
+    @Override
+    public void enterDesignator(CParser.DesignatorContext ctx) {
+
+    }
+
+    @Override
+    public void exitDesignator(CParser.DesignatorContext ctx) {
+
+    }
+
+    @Override
+    public void enterStatement(CParser.StatementContext ctx) {
+
+    }
+
+    @Override
+    public void exitStatement(CParser.StatementContext ctx) {
+
+    }
+
+    @Override
+    public void enterLabeledStatement(CParser.LabeledStatementContext ctx) {
+
+    }
+
+    @Override
+    public void exitLabeledStatement(CParser.LabeledStatementContext ctx) {
+
+    }
+
+    @Override
+    public void enterCompoundStatement(CParser.CompoundStatementContext ctx) {
+
+    }
+
+    @Override
+    public void exitCompoundStatement(CParser.CompoundStatementContext ctx) {
+
+    }
+
+    @Override
+    public void enterBlockItemList(CParser.BlockItemListContext ctx) {
+
+    }
+
+    @Override
+    public void exitBlockItemList(CParser.BlockItemListContext ctx) {
+
+    }
+
+    @Override
+    public void enterBlockItem(CParser.BlockItemContext ctx) {
+
+    }
+
+    @Override
+    public void exitBlockItem(CParser.BlockItemContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionStatement(CParser.ExpressionStatementContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionStatement(CParser.ExpressionStatementContext ctx) {
+
+    }
+
+    @Override
+    public void enterSelectionStatement(CParser.SelectionStatementContext ctx) {
+
+    }
+
+    @Override
+    public void exitSelectionStatement(CParser.SelectionStatementContext ctx) {
+
+    }
+
+    @Override
+    public void enterIterationStatement(CParser.IterationStatementContext ctx) {
+
+    }
+
+    @Override
+    public void exitIterationStatement(CParser.IterationStatementContext ctx) {
+
+    }
+
+    @Override
+    public void enterForCondition(CParser.ForConditionContext ctx) {
+
+    }
+
+    @Override
+    public void exitForCondition(CParser.ForConditionContext ctx) {
+
+    }
+
+    @Override
+    public void enterForDeclaration(CParser.ForDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void exitForDeclaration(CParser.ForDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void enterForExpression(CParser.ForExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitForExpression(CParser.ForExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterJumpStatement(CParser.JumpStatementContext ctx) {
+
+    }
+
+    @Override
+    public void exitJumpStatement(CParser.JumpStatementContext ctx) {
+
+    }
+
+
+
+
+    @Override
+    public void enterDeclarationList(CParser.DeclarationListContext ctx) {
+
+    }
+
+    @Override
+    public void exitDeclarationList(CParser.DeclarationListContext ctx) {
+
+    }
+
+    @Override
+    public void visitTerminal(TerminalNode terminalNode) {
+
+    }
+
+    @Override
+    public void visitErrorNode(ErrorNode errorNode) {
+
+    }
+
+    @Override
+    public void enterEveryRule(ParserRuleContext parserRuleContext) {
+
+    }
+
+    @Override
+    public void exitEveryRule(ParserRuleContext parserRuleContext) {
+
+    }
+}
